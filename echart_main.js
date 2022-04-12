0a1702eed0c45891c819ac85d22f28f9ae285c7f
kinkoocap <103506126+kinkoocap@users.noreply.github.com>
2022-04-12 13:48:47 +0100

    Update echart_main.js

--- echart_main.js
+++ echart_main.js
@@ -5,65 +5,54 @@ var getScriptPromisify = (src) => {
 }
 
 (function() {
-        const prepared = document.createElement('template')
-        prepared.innerHTML = `
+    const prepared = document.createElement('template')
+    prepared.innerHTML = `
       <style>
       </style>
       <div id="root" style="width: 100%; height: 100%;">
       </div>
     `
-        class SamplePieChart extends HTMLElement {
-            constructor() {
-                super()
+    class SamplePieChart extends HTMLElement {
+        constructor() {
+            super()
 
-                this._shadowRoot = this.attachShadow({
-                    mode: 'open'
-                })
-                this._shadowRoot.appendChild(prepared.content.cloneNode(true))
+            this._shadowRoot = this.attachShadow({
+                mode: 'open'
+            })
+            this._shadowRoot.appendChild(prepared.content.cloneNode(true))
 
-                this._root = this._shadowRoot.getElementById('root')
+            this._root = this._shadowRoot.getElementById('root')
 
-                this._props = {}
+            this._props = {}
 
-                this.render()
-            }
+            this.render()
+        }
 
-            onCustomWidgetResize(width, height) {
-                this.render()
-            }
+        onCustomWidgetResize(width, height) {
+            this.render()
+        }
 
 
-            set myDataSource(dataBinding) {
-                this._myDataSource = dataBinding
-                this.render()
-            }
-            customElements.define('com-sap-sample-echartspie_
-                chart_demo_xx ', SamplePieChart)
-            })()
+
+        set myDataSource(dataBinding) {
+            this._myDataSource = dataBindingthis.render()
+        }
 
 
         async render() {
             await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
 
-
             if (!this._myDataSource || this._myDataSource.state !== 'success') {
                 return
             }
-            const dimension = this._myDataSource.metadata.feeds.dimensions.values[0]
-            const measure = this._myDataSource.metadata.feeds.measures.values[0]
-            const data = this._myDataSource.data.map(data => {
+            const dimension = this._myDataSource.metadata.feeds.dimensions.values[0] constmeasure = this._myDataSource.metadata.feeds.measures.values[0] constdata = this._myDataSource.data.map(data => {
                 return {
                     name: data[dimension].label,
                     value: data[measure].raw
                 }
             })
 
-
             const myChart = echarts.init(this._root, 'wight')
-
-            ////////////////////////////////////////////////////////////
-            ///Third comment: Place the third part of the script here///
-            ////////////////////////////////////////////////////////////
             const option = {
                 tooltip: {
                     trigger: 'item'
@@ -104,5 +93,5 @@ var getScriptPromisify = (src) => {
     }
 
 
-    customElements.define('com-sap-sample-echarts-pie_chart_demo_kk', SamplePieChart)
+    customElements.define('com-sap-sample-echarts-pie_chart_demo_xx', SamplePieChart)
 })()

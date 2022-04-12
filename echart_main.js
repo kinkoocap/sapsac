f1d1a6a8452afe6880b00ec4b4e611d38df5baca
kinkoocap <103506126+kinkoocap@users.noreply.github.com>
2022-04-12 13:39:14 +0100

    Update echart_main.js

--- echart_main.js
+++ echart_main.js
@@ -104,5 +104,5 @@ var getScriptPromisify = (src) => {
     }
 
 
-    customElements.define('com-sap-sample-echarts-pie_chart_demo_xx', SamplePieChart)
+    customElements.define('com-sap-sample-echarts-pie_chart_demo_kk', SamplePieChart)
 })()
